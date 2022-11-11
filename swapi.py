25b21503cfbc7b8ef055807b41bf00e758738afa
rufellouis <rufellouis@hotmail.com>
2022-11-10 18:20:24 -0600

    adding original swapi.py file

---
+++ swapi.py
@@ -0,0 +1,11 @@
+import requests
+import json
+
+base_url = "https://swapi.dev/api/people/"
+
+character_id = '4'
+url = base_url + character_id
+response = requests.get(url)
+
+data = response.json()
+print(json.dumps(data, indent=4, sort_keys=True))
